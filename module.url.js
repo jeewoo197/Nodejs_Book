e087221dc9b25a2aba2365ea3c54a37172d41561
jeewoo197 <jeewoo197@naver.com>
2019-10-19 14:25:55 +0900

    querystring module

--- module.url.js
+++ module.url.js
@@ -1,3 +1,4 @@
 var url = require('url');
 var parsedObject = url.parse('http://www.hanbit.co.kr/store/books/look.php?p_code=B4250257160');
-console.log(parsedObject);
+/*console.log(parsedObject);*/
+console.log(querystring.parse(parsedObject));
